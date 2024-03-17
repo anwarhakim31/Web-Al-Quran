b3af2f0ded4ae5eea0983a13558cff37091593bd
Anwar Hakim <anwarhakim31@gmail.com>
2024-03-17 15:15:29 +0700

    connect to 2 rest api

--- surat.js
+++ surat.js
@@ -17,3 +17,23 @@ var variableValue = part[1]; // Get the second part (index 1)
 
 // Output the value
 console.log(variableValue); // Output: 1
+
+const url1 = `https://equran.id/api/v2/surat/`;
+const url2 = `http://api.alquran.cloud/v1/surah/`;
+
+const request1 = fetch(url1 + variableValue).then((response) => {
+  if (!response.ok) {
+    throw new Error(response.statusText);
+  }
+  return response.json();
+});
+const request2 = fetch(url2 + variableValue).then((response) => {
+  if (!response.ok) {
+    throw new Error(response.statusText);
+  }
+  return response.json();
+});
+
+Promise.all([request1, request2]).then(([data1, data2]) => {
+  console.log(data1);
+});
